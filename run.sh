3197743a48cf6cefc5eede3c51d327c5e2635e0d
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-07-19 14:25:06 +0200

    update script to use Java agent via JAVA_TOOL_OPTIONS

--- run.sh
+++ run.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
+# TODO: configure
+# THUNDRA_APIKEY=...
 FUNC_NAME=thundra-localstack
-THUNDRA_APIKEY=...
 AGENT_URL=https://repo.thundra.io/service/local/artifact/maven/redirect?r=thundra-releases&g=io.thundra.agent&a=thundra-agent-lambda-layer&v=LATEST
+USE_AGENT=1
 
 test -e lib/thundra-agent.jar || wget -O lib/thundra-agent.jar "$AGENT_URL"
 
 zip -r lambda.zip lib
 
-awslocal lambda create-function --function-name $FUNC_NAME \
-  --handler io.thundra.agent.lambda.core.handler.ThundraLambdaHandler \
-  --zip-file fileb://./lambda.zip \
-  --runtime java11 \
-  --role test \
-  --timeout 10 \
-  --environment \
-   Variables="{THUNDRA_APIKEY=$THUNDRA_APIKEY,THUNDRA_AGENT_LAMBDA_HANDLER=cloud.localstack.sample.LambdaHandlerWithLib}"
+if [ "$USE_AGENT" = "1" ]; then
+    awslocal lambda create-function --function-name $FUNC_NAME \
+      --handler cloud.localstack.sample.LambdaHandlerWithLib \
+      --zip-file fileb://./lambda.zip \
+      --runtime java11 \
+      --role test \
+      --timeout 10 \
+      --environment \
+       Variables="{THUNDRA_APIKEY=$THUNDRA_APIKEY,JAVA_TOOL_OPTIONS=-javaagent:lib/thundra-agent.jar}"
+else
+     awslocal lambda create-function --function-name $FUNC_NAME \
+       --handler io.thundra.agent.lambda.core.handler.ThundraLambdaHandler \
+       --zip-file fileb://./lambda.zip \
+       --runtime java11 \
+       --role test \
+       --environment Variables="{THUNDRA_APIKEY=$THUNDRA_APIKEY,THUNDRA_AGENT_LAMBDA_HANDLER=cloud.localstack.sample.LambdaHandlerWithLib}" \
+       --tracing-config Mode=Active
+fi
 
 awslocal lambda invoke --function-name $FUNC_NAME --payload '{"test":123}' /tmp/test.out
